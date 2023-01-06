3062fb82156d7d07a79d0cafc3e5b0fdaf79f634
SHUBHAM KUMAR <shubhamkmr06082001@gmail.com>
2023-01-06 20:50:31 +0530

    CREATED

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import Notifications from './screens/Notifications';
+import Settings from './screens/Settings';
+import Profile from './screens/Profile';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen} />
+          <Stack.Screen name='Notifications' component={Notifications} />
+          <Stack.Screen name='Profile' component={Profile} />
+          <Stack.Screen name='Settings' component={Settings} />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'yellow',
+//     textAlign: 'center',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   text: {
+//     color: 'white',
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+// });
